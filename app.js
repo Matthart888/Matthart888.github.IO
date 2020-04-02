b6dcbb93ad725fd605e951ed1e9ae20b6d3d4f07
Em Emm M <emm@Ems-MacBook-Pro.local>
2020-04-01 17:53:41 -0700

    Animation working, missile firing from player

--- app.js
+++ app.js
@@ -13,35 +13,52 @@ console.log("linked")
 
 //create a command so that when the user presses on the left key, the player moves to the left, or moves to the right if they press on the right key
 
+// create an array for the missiles to show up on the screen (in front of the player)
+
+// make an animation for the missile to fly forward
+
+// UNIT COLLISION 
+
 const guardian = {
     top: 500,
     left: 450
 };
 
-let missile = [];
+let missiles = [];
+
+const $bullet = $("<div>").addClass("missile")
 
 const movePlayer = (evt) => {
     console.log(evt)
     if (evt.key === "ArrowLeft"){
         guardian.left -= 10
+        $("#guardian").css("left", guardian.left)
     } else if (evt.key === "ArrowRight"){
         guardian.left += 10
+        $("#guardian").css("left", guardian.left)
     }
-    $("#guardian").css("left", guardian.left)
+    
 }
 
 const shoot = (evt) => {
     if (evt.key === " "){
-        missile.push({
-            left: guardian.left += 15,
+        let misslePositionLeft = guardian.left +3
+        console.log(misslePositionLeft)
+        missiles.push({
+            left: misslePositionLeft,
             top: guardian.top
         })
-        $("#missiles").css("left", missile.left) = ""
-        for (let missile = 0; missile < missile.length; missile ++){
-            $("#missiles").css("left", missile.left).appendTo("#missiles")
+        $("#missiles").empty
+        for (let playerFire = 0; playerFire < missiles.length; playerFire ++){
+            $bullet.css({ "left": missiles[playerFire].left, "top": missiles[playerFire].top}).appendTo("#missiles")
         }
+        $(".missile").animate({
+            top: 0
+        })
     }
 }
+
+
 $(() => {
     console.log("jq linked")
     $(document).on("keydown", movePlayer)
