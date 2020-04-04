b4e57cb5ebf7fec0373928ffd7dda4f3509c9c82
Em Emm M <emm@Ems-MacBook-Pro.local>
2020-04-04 07:50:52 -0700

    Project upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // make sure JS is linked
-console.log("linked")
+// console.log("linked")
 
 // create the opening window with three options: New Game, about, and credits
 
@@ -26,7 +26,22 @@ const guardian = {
 
 let missiles = [];
 
-const $bullet = $("<div>").addClass("missile")
+const badGuys = [
+    {left: 200, top:50},
+    {left: 250, top:50},
+    {left: 300, top:50},
+    {left: 350, top:50},
+    {left: 400, top:50},
+    {left: 450, top:50},
+    {left: 500, top:50},
+    {left: 550, top:50},
+    {left: 600, top:50},
+    {left: 650, top:50},
+    {left: 700, top:50},
+];
+
+
+
 
 const movePlayer = (evt) => {
     console.log(evt)
@@ -36,34 +51,66 @@ const movePlayer = (evt) => {
     } else if (evt.key === "ArrowRight"){
         guardian.left += 10
         $("#guardian").css("left", guardian.left)
-    }
+    };
     
-}
+};
 
-const shoot = (evt) => {
+const drawMissile = (evt) => {
     if (evt.key === " "){
-        let misslePositionLeft = guardian.left +3
-        console.log(misslePositionLeft)
-        missiles.push({
-            left: misslePositionLeft,
-            top: guardian.top
-        })
-        $("#missiles").empty
-        for (let playerFire = 0; playerFire < missiles.length; playerFire ++){
-            $bullet.css({ "left": missiles[playerFire].left, "top": missiles[playerFire].top}).appendTo("#missiles")
-        }
-        $(".missile").animate({
-            top: 0
-        })
-    }
-}
+        // let missilePositionLeft = guardian.left +3
+        // // console.log(missilePositionLeft)
+        //     missiles.push({
+        //         // left: missilePositionLeft,
+        //         // top: guardian.top
+        //     });
+        //     // let currentMissile = $("#missiles").children().toArray()
+            $("#missiles").empty()
+            // for (let playerFire = 0; playerFire < currentMissile.length; playerFire ++){
+                let newMissile = $("<div>").addClass("missile").css({ "left": guardian.left +3, "top": guardian.top})
+                newMissile.appendTo("#missiles")
+                missiles.push(newMissile)
+                newMissile.animate({
+                    top: 0
+                }, {
+                    step: unitCollision
+                }); 
+            };
+};
+
+
+
+// const $baddy = $("<div>").addClass("baddies")
+
+const drawBaddies = () => {
+    for (let numOfBaddies = 0; numOfBaddies < badGuys.length; numOfBaddies ++){
+        $("<div>").addClass("baddies").css({ "left": badGuys[numOfBaddies].left, "top": badGuys[numOfBaddies].top}).appendTo("#baddies")
+    };
+};
+
+const unitCollision = () => 
+{
+
+    for (let numOfBaddies = 0; numOfBaddies < badGuys.length; numOfBaddies ++)
+    {
+        for (let playerFire = 0; playerFire < missiles.length; playerFire ++)
+        {
+            console.log("badGuys[numOfBaddies].top", badGuys[numOfBaddies].top)
+            console.log("missiles[playerFire].position().top", missiles[playerFire].position().top)
+            if (missiles[playerFire].position().top <= badGuys[numOfBaddies].top) 
+            {
+                console.log("HIT")
+            };
+        };
+    };
+};
+
 
 
 $(() => {
-    console.log("jq linked")
+    
     $(document).on("keydown", movePlayer)
-    $(document).on("keydown", shoot)
+    $(document).on("keydown", drawMissile)
+    drawBaddies()
+    // unitCollision()
 
-
-
-})
+});
